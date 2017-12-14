a5253e5cd98a9d1003721fd34e6029b3ebc930b1
yuekai146 <1614917181@qq.com>
2017-12-14 11:27:41 +0800

    add adversarial train to utils.py

--- leak_gan/utils.py
+++ leak_gan/utils.py
@@ -41,7 +41,7 @@ def recurrent_func(f_type='pre'):
             Initialize variables and lists for forward step.
             '''
             h_w_t, c_w_t, h_m_t, c_m_t, last_goal, real_goal, x_t = \
-                init_vars(generator,discriminator, use_cuda)
+                init_vars(generator, discriminator, use_cuda)
             t = 0
             feature_list = []
             delta_feature_list = []
@@ -56,19 +56,19 @@ def recurrent_func(f_type='pre'):
             Perform forward step for pretraining generator and
             dicriminator.
             '''
-            while t < discriminator.seq_len + 1 :
+            while t < seq_len + 1 :
                 '''
                 Extract feature f_t.
                 '''
                 if t == 0:
                     cur_sen = Variable(nn.init.constant(
-                        torch.zeros(batch_size, seq_len)
-                    ), -1).long()
+                        torch.zeros(batch_size, seq_len), -1)
+                    ).long()
                 else:
                     cur_sen = real_data[:, :t]
                     cur_sen = cur_sen.contiguous()
                     cur_sen = F.pad(
-                        cur_sen.view(-1, t + 1), (0, seq_len - t - 1), value=-1
+                        cur_sen.view(-1, t), (0, seq_len - t), value=-1
                     )
                 if use_cuda:
                     cur_sen = cur_sen.cuda(async=True)
@@ -78,7 +78,7 @@ def recurrent_func(f_type='pre'):
                 Generator forward step.
                 '''
                 x_t, h_m_t, c_m_t, h_w_t, c_w_t, last_goal, real_goal,\
-                sub_goal, probs, t = generator(
+                sub_goal, probs, t_ = generator(
                         x_t, f_t, h_m_t, c_m_t, h_w_t, c_w_t, last_goal,
                         real_goal, t, 1.0
                     )
@@ -101,6 +101,7 @@ def recurrent_func(f_type='pre'):
                         delta_feature_list.append(
                             f_t - feature_list[t - step_size]
                         )
+                t = t_
 
             '''
             Post process and return variables needed for calculating loss.
@@ -108,18 +109,127 @@ def recurrent_func(f_type='pre'):
             if len(real_goal_list) == len(delta_feature_list) + 1:
                 real_goal_list = real_goal_list[:-1]
             prediction_list = prediction_list[:-1]
-            real_goal = torch.stack(real_goal_list).permute(1, 0, 2)
-            prediction = torch.stack(prediction_list).permuet(1, 0, 2)
-            delta_feature = torch.stack(delta_feature_list).permute(1, 0, 2)
-            rets = [real_goal, prediction, delta_feature]
-            for ret in rets:
+            real_goal_var = torch.stack(real_goal_list).permute(1, 0, 2)
+            prediction_var = torch.stack(prediction_list).permuet(1, 0, 2)
+            delta_feature_var = torch.stack(delta_feature_list).permute(1, 0, 2)
+            rets = {"real_goal":real_goal_var,
+                    "prediction":prediction_var,
+                    "delta_feature":delta_feature_var}
+            for ret in rets.values():
                 if ret.is_contiguous():
                     ret = ret.contiguous()
             return rets
 
     elif f_type == 'adv':
         def func(model_dict, use_cuda=False, temperature=1.0):
-            pass
+            '''
+            Get generator and discriminator
+            '''
+            generator = model_dict['generator']
+            discriminator = model_dict['discriminator']
+
+            '''
+            Initialize variables and lists for forward step.
+            '''
+            h_w_t, c_w_t, h_m_t, c_m_t, last_goal, real_goal, x_t = \
+                init_vars(generator, discriminator, use_cuda)
+            t = 0
+            feature_list = []
+            delta_feature_list = [] # f_(t+c) - f_t
+            delta_feature_for_worker_list = [] # f_t - f_(t-i)
+            prediction_list = []
+            real_goal_list = []
+            all_goal_list = []
+            gen_token_list = []
+            batch_size = generator.worker.batch_size
+            seq_len = discriminator.seq_len
+            step_size = generator.step_size
+            goal_out_size = generator.worker.goal_out_size
+
+            '''
+            Perform forward step for adversarial training for dicriminator and
+            generator.
+            '''
+            while t < seq_len + 1:
+                '''
+                Extract feature f_t.
+                '''
+                if t == 0:
+                    cur_sen = Variable(nn.init.constant(
+                        torch.zeros(batch_size, seq_len), -1)
+                    ).long()
+                    if use_cuda:
+                        cur_sen = cur_sen.cuda(async=True)
+                else:
+                    cur_sen = torch.stack(gen_token_list).permute(1, 0)
+                    cur_sen = F.pad(
+                        cur_sen, (0, seq_len - t), value=-1
+                    )
+                f_t = discriminator(cur_sen)["feature"]
+                '''
+                Generator forward step.
+                '''
+                x_t, h_m_t, c_m_t, h_w_t, c_w_t, last_goal, real_goal,\
+                sub_goal, probs, t_ = generator(
+                        x_t, f_t, h_m_t, c_m_t, h_w_t, c_w_t, last_goal,
+                        real_goal, t, temperature
+                    )
+                if t % step_size == 0:
+                    real_goal = last_goal
+                    last_goal = Variable(torch.zeros(
+                        batch_size, goal_out_size
+                    ))
+                    if use_cuda:
+                        last_goal = last_goal.cuda(async=True)
+                    real_goal_list.append(real_goal)
+
+                '''
+                Store needed information for caculating loss function
+                '''
+                feature_list.append(f_t)
+                prediction_list.append(probs)
+                if t > 0:
+                    if t % step_size == 0:
+                        delta_feature_list.append(
+                            f_t - feature_list[t - step_size]
+                        )
+                        delta_feature_for_worker_list.append(
+                            f_t - feature_list[t - step_size]
+                        )
+                    else:
+                        delta_feature_for_worker_list.append(
+                            f_t - feature_list[t - t%step_size]
+                        )
+                    all_goal_list.append(real_goal)
+                gen_token_list.append(x_t)
+                t = t_
+
+            '''
+            Post process and return variables.
+            '''
+            if len(real_goal_list) == len(delta_feature_list) + 1:
+                real_goal_list = real_goal_list[:-1]
+            prediction_list = prediction_list[:-1]
+            real_goal_var = torch.stack(real_goal_list).permute(1, 0, 2)
+            all_goal_var = torch.stack(all_goal_list).permute(1, 0, 2)
+            prediction_var = torch.stack(prediction_list).permuet(1, 0, 2)
+            delta_feature_var = torch.stack(
+                delta_feature_list
+            ).permute(1, 0, 2)
+            gen_token_var = torch.stack(gen_token_list).permute(1, 0)
+            delta_feature_for_worker_var = torch.stack(
+                delta_feature_for_worker_list
+            ).permute(1, 0, 2)
+            rets = {"real_goal":real_goal_var,
+                    "all_goal":all_goal_var,
+                    "prediction":prediction_var,
+                    "delta_feature":delta_feature_var,
+                    "delta_feature_for_worker":delta_feature_for_worker_var,
+                    "gen_token":gen_token_var}
+            for ret in rets.values():
+                if ret.is_contiguous():
+                    ret = ret.contiguous()
+            return rets
 
     elif f_type == 'rollout':
         pass
