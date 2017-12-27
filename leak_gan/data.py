5a4cb17457cbb6bf405138f1664c7570ca088916
yuekai146 <1614917181@qq.com>
2017-12-27 12:03:16 +0800

    add label for dis_data_set.

--- leak_gan/data.py
+++ leak_gan/data.py
@@ -19,13 +19,20 @@ class Dis_Data_Set(Dataset):
     def __init__(self, positive_filepath, negative_filepath):
         pos_data = np.load(positive_filepath)
         neg_data = np.load(negative_filepath)
+        pos_label = np.array([1 for _ in pos_data])
+        neg_label = np.array([0 for _ in neg_data])
         self.data = np.concatenate([pos_data, neg_data])
+        self.label = np.concatenate([pos_label, neg_label])
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        return torch.from_numpy(self.data[idx]).long()
+        data = torch.from_numpy(self.data[idx]).long()
+        label = torch.nn.init.constant(
+              torch.zeros(1), int(self.label[idx])
+          ).long()
+        return {"data":data, "label":label}
 
 def real_data_loader(filepath, batch_size, shuffle, num_workers,
                      pin_memory):
